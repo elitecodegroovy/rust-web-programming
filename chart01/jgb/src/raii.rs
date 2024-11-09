e50d134c1e1d6eaf2d8632fe454ad7af7ae2944d
elitegroovycode <elite_jigang@163.com>
2024-11-09 22:17:07 +0800

    feat: rust thread and join case

--- chart01/jgb/src/raii.rs
+++ chart01/jgb/src/raii.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 // raii.rs
 #[allow(dead_code)]
 pub fn create_box() {
@@ -5,6 +7,55 @@ pub fn create_box() {
     let _box1 = Box::new(3i32);
 
     // `_box1` is destroyed here, and memory gets freed
+}
+
+#[allow(dead_code)]
+pub fn do_thread() {
+    let mut children_threads = vec![];
+
+    let count = 5;
+    for i in 0..count {
+        children_threads.push(thread::spawn(move || {
+            println!(">>> thread i {}", i)
+        }))
+    }
+    for child_thread in children_threads {
+        let _ = child_thread.join();
+    }
+}
+
+/// map reduce thread scheduling
+#[allow(dead_code)]
+pub fn do_map_reduce() {
+    let data_number = "1234567890
+    12345678901234567890
+    123456789012345678901234567890
+    1234567890123456789012345678901234567890
+    12345678901234567890123456789012345678901234567890
+    123456789012345678901234567890123456789012345678901234567890
+    1234567890123456789012345678901234567890123456789012345678901234567890
+    12345678901234567890123456789012345678901234567890123456789012345678901234567890
+    123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+    1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+    12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+    123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+    1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+    12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
+
+    let mut reduce_vector = vec![];
+
+    let children_numbers = data_number.split_ascii_whitespace();
+    for (i, line_number) in children_numbers.enumerate() {
+        reduce_vector.push(thread::spawn(move || -> u32 {
+            let result = line_number.chars().map(|c| c.to_digit(10).expect("expect a number value")).sum();
+            println!(">>> line index {}, result : {}", i, result);
+            return result
+        }))
+    }
+
+    let final_result :u32 = reduce_vector.into_iter().map(|s| s.join().unwrap()).sum::<u32>();
+
+    println!(">>> final sum result: {}", final_result)
 }
 
 
@@ -20,5 +71,7 @@ mod tests {
             create_box();
         }
 
+        do_thread();
+        do_map_reduce();
     }
 }

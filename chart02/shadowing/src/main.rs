9796e95bb61d8b04e76801caffbf11b8ab083b1b
elitegroovycode <elite_jigang@163.com>
2025-05-20 15:48:31 +0800

    feat: rust data type and loop

--- chart02/shadowing/src/main.rs
+++ chart02/shadowing/src/main.rs
@@ -32,4 +32,59 @@ fn main() {
             break;
         }
     }
+
+    let mut cc = 0u8;
+    while (cc != 10) {
+        cc += 1;
+        if cc == 3 {
+            println!("cc 是 3，跳过接下来的业务逻辑，执行跳到 while 声明处继续执行。");
+            // Skip the rest of this iteration
+            continue;
+        }
+    }
+    println!("cc 是 10 ，停止执行");
+
+    for n in 1..10 {
+        if n % 2 == 0 {
+            println!("偶数: {} ", n);
+        } else {
+            println!("基数: {}", n);
+        }
+    }
+
+    for n in 1..=10 {
+        if n % 2 == 0 {
+            println!(">偶数: {} ", n);
+        } else {
+            println!(">基数: {}", n);
+        }
+    }
+
+    let car_names = vec!["BYD", "赛力斯", "特斯拉"];
+
+    for car_name in car_names.iter() {
+        match car_name {
+            &"BYD" => println!(" 比亚迪车!"),
+            _ => println!("车： {}", car_name),
+        }
+    }
+    println!("车品牌: {:?}", car_names);
+
+    let car_names2 = vec!["BYD", "赛力斯", "特斯拉"];
+    for car_name in car_names2.into_iter() {
+        match car_name {
+            "BYD" => println!(" 比亚迪车!"),
+            _ => println!("车： {}", car_name),
+        }
+    }
+    // car_names2 不再可用
+    // println!("车品牌: {:?}", car_names2);
+    let mut car_names3 = vec!["BYD", "赛力斯", "特斯拉"];
+    for car_name in car_names3.iter_mut() {
+        *car_name = match car_name {
+            &mut "BYD" => "BYD 品牌!",
+            _ => "其他车品牌",
+        }
+    }
+    println!("车品牌: {:?}", car_names3);
 }

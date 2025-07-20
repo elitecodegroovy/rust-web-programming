c09667a5e851d13a838ddd602413704437f4c40c
elitegroovycode <elite_jigang@163.com>
2025-07-20 23:52:55 +0800

    feat: rust data type and loop

--- chart02/shadowing/src/main.rs
+++ chart02/shadowing/src/main.rs
@@ -1,8 +1,10 @@
+use std::fmt::format;
 
 const MAX_CONCURRENCY_NUM: u32 = 1_000;
 
 
 fn do_match_value() {
+
 
     // match array
     do_match_array();
@@ -12,9 +14,219 @@ fn do_match_value() {
     do_match_ref();
     // structs
     do_match_struct();
+
+    // basic match
+    do_basic_match();
 }
 
-fn do_match_struct() {
+enum ColorSetting {
+    RED,
+    YELLOW,
+    BLUE,
+    GREEN,
+}
+
+fn name_in_color_setting(s: ColorSetting) -> &'static str {
+    match s {
+        ColorSetting::RED => "red",
+        ColorSetting::YELLOW => "yellow",
+        ColorSetting::BLUE => "blue",
+        ColorSetting::GREEN => "green",
+    }
+}
+
+fn get_number(n: i32) -> &'static str {
+    match n {
+        1 => "ONE",
+        2 | 3 => "TWO OR THREE",
+        4..=10 => "BETWEEN 4 AND 10",
+        _ => "Something else number",
+    }
+}
+
+
+fn do_basic_match() {
+    println!("color: {} ", name_in_color_setting(ColorSetting::RED));
+    println!("color: {} ", name_in_color_setting(ColorSetting::GREEN));
+
+    do_match_binding();
+    println!("Nested: {}", get_str_option(Some(Some(100))));
+    println!("Nested: {}", get_str_option(Some(None)));
+    println!("Nested: {}", get_str_option(None));
+
+    let op = Some(9);
+    let number = match op {
+        Some(i) => i,
+        _ => 0
+    };
+    println!("match some n: {}", number);
+    do_if_let();
+    do_let_else();
+    do_while_let();
+
+    fn get_value_or_default(opt: Option<i32>) -> i32 {
+        opt.unwrap_or_else(|| 0)
+    }
+    println!(">>> {}", get_value_or_default(Some(1)));
+
+    do_methods();
+}
+
+fn do_methods() {
+    struct Counter {
+        count: Option<i32>,
+    }
+
+    impl Counter {
+        // Instance method to get count or default
+        fn get_count(&self) -> i32 {
+            self.count.unwrap_or_else(|| 0)
+        }
+    }
+    let counter1 = Counter { count: Some(5) };
+    let counter2 = Counter { count: None };
+    println!("counter 1: {}", counter1.get_count());
+    println!("counter 2: {}", counter2.get_count());
+
+    do_closures();
+}
+
+fn do_closures() {
+    let process_option = |opt: Option<i32>| -> i32 {
+        match opt {
+            Some(value) => value * 2,
+            None => 0,
+        }
+    };
+
+    let some = Some(9);
+    let none: Option<i32> = None;
+    println!("Some: {}", process_option(some)); // Output: Some: 10
+    println!("None: {}", process_option(none)); // Output: None: 0
+
+    struct Processing {
+        value: Option<i32>,
+    }
+
+    impl Processing {
+        fn apply<F>(&self, f: F) -> i32
+        where
+            F: Fn(Option<i32>) -> i32,
+        {
+            f(self.value)
+        }
+    }
+
+    let processing = Processing { value: Some(9) };
+    let double = |opt: Option<i32>| match opt {
+        Some(v) => v * 2,
+        None => 0,
+    };
+    println!("processor result: {}", processing.apply(double));
+
+    let mut opt = Some(String::from("Rust"));
+    let consume = |opt: Option<String>| {
+        let Some(s) = opt else { return "No string".to_string(); };
+        format!("Consumed: {}", s)
+    };
+
+    println!("Result: {}", consume(opt)); // Output: Result: Consumed: Hello
+    // println!("Data: {:?}", opt); // Error: opt was moved into closure
+
+    let opt_some = vec![Some(1), None, Some(3)];
+    let process = |opt: Option<i32>| opt.unwrap_or(0);
+    let r: Vec<i32> = opt_some.into_iter().map(process).collect();
+    println!("vec numbers: {:?}", r); // Output: Results: [1, 0, 3]
+
+    fn apply_to_option<T, F>(opt: Option<T>, f: F) -> Option<T>
+    where
+        F: Fn(T) -> T,
+    {
+        opt.map(f)
+    }
+
+    let double = |x: i32| x * 2;
+    let result = apply_to_option(Some(9), double);
+    println!("Result: {:?}", result); // Output: Result: Some(10)
+    let none: Option<i32> = None;
+    println!("None: {:?}", apply_to_option(none, double)); // Output: None: None
+
+    // map method
+    let numbers = vec![10, 20, 30];
+    let doubled: Vec<i32> = numbers.iter().map(|x| x * 2).collect();
+    println!("map vec values: {:?}", doubled); // Outputs: [20, 40, 60]
+
+    let mut numbers1 = vec![11, 12, 13, 14];
+    let evens: Vec<i32> = numbers1.into_iter().filter(|x| *x % 2 == 0).collect();
+    println!("{:?}", evens); // Outputs: [12, 14]
+
+    let numbers2 = vec![11, 12, 13];
+    let sum = numbers2.iter().fold(0, |acc, x| acc + x);
+    println!("{}", sum); // Outputs: 36
+
+    let numbers3 = vec![1, 2, 3, 4, 5];
+    let result: Vec<i32> = numbers3
+        .into_iter()
+        .filter(|x| x % 2 == 0) // Keep even numbers
+        .map(|x| x * 3)          // Triple them
+        .collect();
+    println!("{:?}", result); // Outputs: [6, 12]
+}
+
+fn do_while_let() {
+    let mut op = Some(9);
+
+    // This reads: "while `let` destructures `op`
+    // into `Some(i)`, evaluate the block (`{}`). Else `break`.
+    while let Some(i) = op {
+        if i > 9 {
+            println!("Greater than 9, quit!");
+            op = None;
+        } else {
+            println!("`i` is `{:?}`. Try again.", i);
+            op = Some(i + 1);
+        }
+    }
+}
+
+fn do_let_else() {
+    fn process_value_with_double(opt: Option<i32>) -> i32 {
+        let Some(v) = opt else { return 0};
+        v * 2
+    }
+    let some_v = Some(9);
+    let no_v: Option<i32> = None;
+    println!(" Some {}", process_value_with_double(some_v));
+    println!(" None: {}", process_value_with_double(no_v));
+}
+
+fn do_if_let() {
+    let op: Option<i32> = None;
+    if let Some(i) = op {
+        println!("match some n: {}", i);
+    } else {
+        println!("match some n: {}", 0);
+    }
+}
+
+fn get_str_option(value: Option<Option<i32>>) -> i32 {
+    match value {
+        Some(Some(num)) => num,
+        Some(None) => 0,
+        None => -1
+    }
+}
+
+fn do_match_binding() {
+    let number = 8;
+    let result = match number {
+        n if n % 2 == 0 => format!(" {} 是 偶数", n),
+        n => format!(" {} 是奇数", n)
+    };
+    println!("{}", result); //
+}
+
+fn do_match_struct() {;
     struct Foo {
         x: (u32, u32),
         y: u32,

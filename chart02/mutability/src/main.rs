a0cb8857d25aebc7007a0e017d963e44eca2cfd9
elitegroovycode <elite_jigang@163.com>
2025-08-02 20:38:44 +0800

    feat: rust data type and loop

--- chart02/mutability/src/main.rs
+++ chart02/mutability/src/main.rs
@@ -7,4 +7,11 @@ fn main() {
 
     b += 10;
     println!(" >>> a={}, b={}", a, b);
+
+    let mut s = String::with_capacity(10_000_000);
+    for i in 0..10_000_000 {
+        s.push('.')
+    }
+    println!("x len {}", s.len());
+
 }

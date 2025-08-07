f361d6caab5ab18ca8e4d53a250451c448edcf99
elitegroovycode <elite_jigang@163.com>
2025-08-07 17:31:41 +0800

    feat: async / await Fn

--- chart02/async_tokio/src/main.rs
+++ chart02/async_tokio/src/main.rs
@@ -1,29 +1,26 @@
 use tokio::time::Duration;
 
-async fn fetch_data() -> Result<String, reqwest::Error> { // ①
-    tokio::time::sleep(Duration::from_secs(2)).await; // ②
-    Ok::<String, _>("Data fetched successfully".to_owned()) // ③
+async fn fetch_data() -> Result<String, reqwest::Error> { 
+    tokio::time::sleep(Duration::from_secs(2)).await; 
+    Ok::<String, _>("Loading data successfully".to_owned()) 
 }
 
-async fn process_data(data: String) { // ④
-    println!("Processing: {}", data); // ⑤
+async fn process_data(data: String) { 
+    println!("Processing: {}", data); 
 }
 
 async fn main_async_workflow() { // ⑥
-    let fetch_task = tokio::spawn(fetch_data()); // ⑦
-    let process_task = tokio::spawn(process_data("Sample Data".to_owned())); // ⑧
-    let fetch_result = fetch_task.await.expect("Failed to fetch data"); // ⑨
-    let _ = process_task.await; // ⑩
-    println!("Fetch Result: {:?}", fetch_result); // ⑪
+    let fetch_task = tokio::spawn(fetch_data()); 
+    let process_task = tokio::spawn(process_data("Sample Data".to_owned())); 
+    let fetch_result = fetch_task.await.expect("Failed to fetch data"); 
+    let _ = process_task.await; 
+    println!("Fetch Result: {:?}", fetch_result); 
 
     println!("successfully exit app!")
 }
 
 
-#[tokio::main] // ⑫
+#[tokio::main] 
 async fn main() {
-    main_async_workflow().await; // ⑬
+    main_async_workflow().await; 
 }
-// Output
-// Processing: Sample Data
-// Fetch Result: Ok(“Data fetched successfully”)

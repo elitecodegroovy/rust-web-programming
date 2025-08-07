90ad5e2de4ab684949c3f8b6c91337ce3b497db1
elitegroovycode <elite_jigang@163.com>
2025-08-07 16:55:34 +0800

    feat: chart03

--- chart03/fast-api/src/main.rs
+++ chart03/fast-api/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Rust Web Programming")
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Listening on port 8080");
+    HttpServer::new(|| {
+        App::new()
+            .route("/hi", web::get().to(hello))
+    })
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
 }
